d7cf0d4a2d3538334790e212a30b716adf96f7d0
Graham Gilchrist <git@grahamgilchrist.com>
2017-06-08 15:16:36 +0100

    sort upgrades alphabetically

--- js-src/modules/models/upgrades.js
+++ js-src/modules/models/upgrades.js
@@ -3,9 +3,21 @@
 var _ = require('lodash');
 var upgrades = require('../../generated/upgrades');
 
+var sortedUpgrades = upgrades.sort(function (a, b) {
+    if (a.name < b.name) {
+        return -1;
+    }
+    if (a.name > b.name) {
+        return 1;
+    }
+
+    // names must be equal
+    return 0;
+});
+
 // key upgrades by type
 var keyedUpgrades = {};
-_.each(upgrades, function (upgrade) {
+_.each(sortedUpgrades, function (upgrade) {
 
     // Remove imperial and scum cards
     if (upgrade.faction === 'Galactic Empire' || upgrade.faction === 'Scum and Villainy') {
@@ -24,6 +36,6 @@ _.each(upgrades, function (upgrade) {
 });
 
 module.exports = {
-    all: upgrades,
+    all: sortedUpgrades,
     keyed: keyedUpgrades
 };
