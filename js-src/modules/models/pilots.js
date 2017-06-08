e4b1339b9c87a1fbce958ffb6f3bd6a4dd80457f
Graham Gilchrist <git@grahamgilchrist.com>
2017-06-08 17:23:57 +0100

    sort pilots and ships alphabetically

--- js-src/modules/models/pilots.js
+++ js-src/modules/models/pilots.js
@@ -3,8 +3,20 @@
 var _ = require('lodash');
 var pilots = require('../../generated/pilots');
 
+var sortedPilots = pilots.sort(function (a, b) {
+    if (a.name < b.name) {
+        return -1;
+    }
+    if (a.name > b.name) {
+        return 1;
+    }
+
+    // names must be equal
+    return 0;
+});
+
 // key upgrades by type
-var rebelPilots = _.filter(pilots, function (pilot) {
+var rebelPilots = _.filter(sortedPilots, function (pilot) {
     return pilot.faction === 'Rebel Alliance';
 });
 var pilotsWithAbilities = _.filter(rebelPilots, function (pilot) {
--- js-src/modules/models/shipCards.js
+++ js-src/modules/models/shipCards.js
@@ -2,4 +2,16 @@
 
 var shipCards = require('../../generated/ships');
 
-module.exports = shipCards;
+var sortedShips = shipCards.sort(function (a, b) {
+    if (a.name < b.name) {
+        return -1;
+    }
+    if (a.name > b.name) {
+        return 1;
+    }
+
+    // names must be equal
+    return 0;
+});
+
+module.exports = sortedShips;
