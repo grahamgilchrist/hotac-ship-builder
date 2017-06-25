9e7cf10de768a5a3deb5f5bed1a42584662cb49d
Graham Gilchrist <git@grahamgilchrist.com>
2017-06-25 14:58:04 +0100

    Add required callsign and player name fields with errors if empty

--- js-src/modules/views/newView.js
+++ js-src/modules/views/newView.js
@@ -13,12 +13,36 @@ module.exports = {
     },
     bindStartButton: function () {
         $('#start-build').on('click', function () {
+            var callsign = $('#callsign').val().trim();
+            var playerName = $('#player-name').val().trim();
             var chosenShipId = $('#starting-ships select').val();
-            events.trigger('view.new.start', {
-                shipId: chosenShipId,
-                callsign: $('#callsign').val(),
-                playerName: $('#player-name').val()
-            });
+            var formErrors = [];
+
+            if (!callsign || callsign.length === 0) {
+                formErrors.push({
+                    fieldId: 'callsign',
+                    message: 'Please enter a callsign'
+                });
+            }
+
+            if (!playerName || playerName.length === 0) {
+                formErrors.push({
+                    fieldId: 'player-name',
+                    message: 'Please enter a player name'
+                });
+            }
+
+            module.exports.clearFormErrors();
+            if (formErrors.length > 0) {
+                module.exports.setFormErrors(formErrors);
+            } else {
+                events.trigger('view.new.start', {
+                    shipId: chosenShipId,
+                    callsign: callsign,
+                    playerName: playerName
+                });
+            }
+
         });
     },
     renderStartingShipsList: function () {
@@ -43,11 +67,27 @@ module.exports = {
     },
     reset: function () {
         module.exports.resetStartingShipsList();
-        $('#callsign').val();
-        $('#player-name').val();
+        $('#callsign').val('');
+        $('#player-name').val('');
     },
     resetStartingShipsList: function () {
         $('#starting-ships option').first().prop('selected', 'selected');
+    },
+    clearFormErrors: function () {
+        $('.new .form-group').removeClass('error');
+        $('.new .form-group .error').remove();
+    },
+    setFormErrors: function (formErrors) {
+
+        _.forEach(formErrors, function (error) {
+
+            var $errorField = $('#' + error.fieldId);
+            if ($errorField.length > 0) {
+                var $formField = $errorField.closest('.form-group');
+                $formField.addClass('error');
+                $formField.append('<div class="error"><div class="error-pad"></div><div class="message">' + error.message + '</div></div>');
+            }
+        });
     },
     hide: function () {
         $('.new').removeClass('active');
