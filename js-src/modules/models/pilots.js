8450c0ea3a9cdad28425376b62c447bc78fd3134
Graham Gilchrist <git@grahamgilchrist.com>
2017-09-06 18:16:12 +0100

    filter out huge ship pilots from abilities list

--- js-src/modules/models/pilots.js
+++ js-src/modules/models/pilots.js
@@ -2,6 +2,13 @@
 
 var _ = require('lodash');
 var pilots = require('../../generated/pilots');
+var shipsData = require('../../generated/ships');
+
+var getShipDataByName = function (shipName) {
+    return _.find(shipsData, function (shipData) {
+        return shipData.name === shipName;
+    });
+};
 
 var sortedPilots = pilots.sort(function (a, b) {
     if (a.skill < b.skill) {
@@ -24,15 +31,26 @@ var sortedPilots = pilots.sort(function (a, b) {
 
 // TODO: filter out pilots from large ships
 
-// key upgrades by type
+// Filter to only include rebel pilots
 var rebelPilots = _.filter(sortedPilots, function (pilot) {
     return pilot.faction === 'Rebel Alliance' || pilot.faction === 'Resistance';
 });
+// filter to only include pilots with abilities (e.g. get rid of generics)
 var pilotsWithAbilities = _.filter(rebelPilots, function (pilot) {
     return pilot.text;
 });
+// filter to rmeove pilots for huge ships
+var nonHugePilots = _.filter(pilotsWithAbilities, function (pilot) {
+    var shipName = pilot.ship;
+    var matchingShip = getShipDataByName(shipName);
+    if (matchingShip.size === 'huge') {
+        return false;
+    }
+    return true;
+});
 
+// key upgrades by type
 module.exports = {
     allRebels: rebelPilots,
-    pilotsWithAbilities: pilotsWithAbilities
+    pilotsWithAbilities: nonHugePilots
 };
