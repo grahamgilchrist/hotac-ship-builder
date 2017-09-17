bf5d2cc25c11fc3682b09758ef24413628ff8cbb
Graham Gilchrist <git@grahamgilchrist.com>
2017-09-17 12:02:25 +0100

    Fix card image responsive size for ipad portrait

--- js-src/modules/views/summary.js
+++ js-src/modules/views/summary.js
@@ -25,7 +25,7 @@ module.exports = {
         var imageUrl = '/components/xwing-data/images/' + upgrade.image;
         var iconString = module.exports.getIconString(upgrade.slot);
         $li.append('<div class="preview" data-featherlight="' + imageUrl + '">' + iconString + '<span class="name">' + upgrade.name + '</span><i class="material-icons icon-preview">zoom_in</i></div>');
-        $li.append('<div class="full"><img src="' + imageUrl + '" alt="Card image for ' + upgrade.name + '"></div>');
+        $li.append('<div class="full" data-featherlight="' + imageUrl + '"><img src="' + imageUrl + '" alt="Card image for ' + upgrade.name + '"></div>');
 
         return $li;
     },
@@ -34,7 +34,7 @@ module.exports = {
         var iconString = module.exports.getIconString('Elite');
         var escapedText = ability.text.replace(/"/g, '&quot;');
         $li.append('<div class="preview" data-featherlight-type="text" data-featherlight-variant="preview-pilot-ability" data-featherlight="' + escapedText + '">' + iconString + '<span class="name">' + ability.name + '</span><i class="material-icons icon-preview">zoom_in</i></div>');
-        $li.append('<div class="full ability"><span class="title">' + ability.name + '</span><span class="name">Ability</span><span class="text">' + escapedText + '</span></div>');
+        $li.append('<div class="full ability"><div class="ability-inner"><span class="title">' + ability.name + '</span><span class="name">Ability</span><span class="text">' + escapedText + '</span></div></div>');
 
         return $li;
     },
