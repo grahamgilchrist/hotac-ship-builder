d5d6971fa415177f70e9c8ccd98a2c17be4cc79c
Graham Gilchrist <git@grahamgilchrist.com>
2017-10-05 01:18:41 +0100

    Add PS and cost to ability cards

--- js-src/modules/views/abilityCard.js
+++ js-src/modules/views/abilityCard.js
@@ -5,7 +5,7 @@ var $ = require('jquery');
 module.exports = {
     render: function (ability) {
         var escapedText = ability.text.replace(/"/g, '&quot;');
-        var $card = $('<div class="ability-card"><div class="ability-inner"><span class="title">' + ability.name + '</span><span class="name">Ability</span><span class="text">' + escapedText + '</span></div></div>');
+        var $card = $('<div class="ability-card"><div class="ability-inner"><span class="title">' + ability.name + '</span><span class="name">Ability  (PS ' + ability.skill + ')</span><span class="text">' + escapedText + '</span><span class="cost">' + ability.skill + '</span></div></div>');
 
         return $card;
     }
