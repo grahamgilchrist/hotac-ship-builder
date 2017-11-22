3871a3160ecc5dbc19f981c2346cfc9da896db52
Graham Gilchrist <git@grahamgilchrist.com>
2017-11-22 23:31:31 +0000

    Use material lite snackbar instead of custom JS

--- js-src/modules/views/message.js
+++ js-src/modules/views/message.js
@@ -5,28 +5,25 @@ var events = require('../controllers/events');
 
 module.exports = {
     renderMessage: function (xpItem, xpItemIndex) {
-        var $messageWrapper = $('<div class="message">');
-        var $message = $('<span>' + xpItem.label() + ' (' + xpItem.cost() + 'XP)</span>');
-        var $button = $('<button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">Undo</button>');
+        var $message = $('#message');
+        var messageElement = $message.get(0);
+        var messageText = xpItem.label() + ' (' + xpItem.cost() + 'XP)';
 
-        $button.on('click', function () {
+        var onClick = function () {
+            messageElement.MaterialSnackbar.cleanup_();
             events.trigger('view.xpHistory.revert', xpItemIndex - 1);
-        });
+        };
 
-        $messageWrapper.append($message);
-        $messageWrapper.append($button);
-        module.exports.clear();
-        $('#message').append($messageWrapper);
-        $('#message').slideDown();
-
-        var timoutSeconds = 5;
-        setTimeout(function () {
-            $('#message').slideUp(undefined, function () {
-                module.exports.clear();
-            });
-        }, timoutSeconds * 1000);
+        var data = {
+            message: messageText,
+            actionHandler: onClick,
+            actionText: 'Undo'
+        };
+        messageElement.MaterialSnackbar.showSnackbar(data);
     },
     clear: function () {
-        $('#message').empty();
+        var $message = $('#message');
+        var messageElement = $message.get(0);
+        messageElement.MaterialSnackbar.cleanup_();
     }
 };
