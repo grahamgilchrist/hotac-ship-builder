a841fa21eb83aeed77d85185b3a80f3f2a06e154
Graham Gilchrist <mail@grahamgilchrist.com>
2024-04-03 18:48:24 +0100

    copy assets from npm packages instead of bower

---
+++ node/copyXwingAssets.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const gulp = require('gulp');
+const rename = require('gulp-rename');
+const path = require('path');
+const fs = require('fs-extra')
+const assetPackageNames = [
+    'xwing-data',
+    'xwing-miniatures-font'
+];
+
+const gulpPath = process.cwd();
+
+module.exports = function () {
+    gulp.task('copyXwingAssets', function () {
+        assetPackageNames.forEach(function (packageName) {
+            const srcPackagePath = require.resolve(packageName);
+            const assetPath = gulpPath + '/app/components/' + packageName
+            fs.copySync(srcImagePath, newImagePath)
+        });
+    });
+};
